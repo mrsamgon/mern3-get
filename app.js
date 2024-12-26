76aaa558ffeda225625340e7645da5a12e71b524
mrsamgon <itsnotmethenwho23402@gmail.com>
2024-12-26 11:42:02 +0400

    chnageg

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const cors = require('cors')
 
 app.use(cors(
     {
-        origin :"https://saro-rho.vercel.app/"
+        origin : ["http://localhost:5173", "https://saro-rho.vercel.app/"]
     }
 ))
 
